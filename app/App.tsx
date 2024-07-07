f2e68ab5d9d8d8f8c804d201d7ff1afe85427713
Waseem <wyameen152@gmail.com>
2024-07-07 13:49:39 +0500

    V-1

---
+++ app/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+function App(): React.JSX.Element {
+
+  return (
+    <Text>Hi</Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
